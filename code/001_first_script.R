0b102053674ac3dc13c030e3924e734169bfbeb2
unknown <localadmin@bio22-2211.ad.uni-bielefeld.de>
2023-12-07 13:59:36 +0100

    First commit

---
+++ code/001_first_script.R
@@ -0,0 +1,9 @@
+# this is an example first script
+
+# simulating two variables
+x <- rnorm(mean=34, sd = 12, n = 1000)
+y <- rnorm(mean=17, sd = 6, n = 1000)
+
+# and testing if the are correlated
+
+cor.test(x,y)
