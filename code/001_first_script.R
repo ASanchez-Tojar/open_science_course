2e9624ff1f5d9d22736d0f598152f820245a3f27
ASanchez-Tojar <alfredo.tojar@gmail.com>
2023-12-08 05:28:30 +0100

    adding one categorical predictor to the simulated dataset

--- code/001_first_script.R
+++ code/001_first_script.R
@@ -1,15 +1,19 @@
 # this is an example first script that simulates a dataset
 
 # simulating two variables
-x <- rnorm(mean=34, sd = 12, n = 1000)
-y <- rnorm(mean=17, sd = 6, n = 1000)
+x <- rnorm(mean=34, sd = 12, n = 100)
+y <- rnorm(mean=17, sd = 6, n = 100)
+
+# adding a categorical predictor
+predictor <- c(rep("control",length(x)/2),
+               rep("treatment",length(x)/2))
 
 
 # one change
 
 # two changes
 
-data.sim <- data.frame(cbind(x,y))
+data.sim <- data.frame(cbind(x,y,predictor))
 
 write.csv(data.sim,
           file = "data/our_simulated_data.csv",
