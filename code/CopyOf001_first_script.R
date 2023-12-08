48b39cbd1cfc2e2c020b35d41f5fea6147f30223
Johanna <johannamhein@googlemail.com>
2023-12-08 15:25:47 +0100

    add test things

---
+++ code/CopyOf001_first_script.R
@@ -0,0 +1,20 @@
+# this is an example first script that simulates a dataset
+
+# simulating two variables
+x <- rnorm(mean=34, sd = 12, n = 100)
+y <- rnorm(mean=17, sd = 6, n = 100)
+
+# adding a categorical predictor
+predictor <- c(rep("control",length(x)/2),
+               rep("treatment",length(x)/2))
+
+
+# two changes
+
+# three changes
+
+data.sim <- data.frame(cbind(x,y,predictor))
+
+write.csv(data.sim,
+          file = "data/our_simulated_data.csv",
+          row.names = FALSE)
