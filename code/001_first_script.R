2d0f934703b6d535dcb65c1c930d47dabe361291
ASanchez-Tojar <alfredo.tojar@gmail.com>
2023-12-08 05:20:52 +0100

    adding more comments

--- code/001_first_script.R
+++ code/001_first_script.R
@@ -1,12 +1,9 @@
-# this is an example first script
+# this is an example first script that simulates a dataset
 
 # simulating two variables
 x <- rnorm(mean=34, sd = 12, n = 1000)
 y <- rnorm(mean=17, sd = 6, n = 1000)
 
-# and testing if the are correlated
-
-cor.test(x,y)
 
 # one change
 
